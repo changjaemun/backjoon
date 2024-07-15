9474ebb414fb86e8e181506be5837bfd9a71ea94
Ito <107340769+changjaemun@users.noreply.github.com>
2024-07-15 19:01:57 +0900

    [Bronze II] Title: Hashing, Time: 12 ms, Memory: 79512 KB, Score: 100 point -BaekjoonHub

--- Swift/백준/Bronze/15829. Hashing/Hashing.swift
+++ Swift/백준/Bronze/15829. Hashing/Hashing.swift
@@ -7,9 +7,9 @@ let str:[String] = Array(readLine()!).map{String($0)}
 func hash(_ l:Int) -> Int{
     var answer:[Int] = []
     for i in 0..<l{
-        answer.append(alphabet.firstIndex(of: str[i])! * r(i))
+        answer.append((alphabet.firstIndex(of: str[i])! * r(i)) % 1234567891)
     }
-    return answer.reduce(0, +)
+    return answer.reduce(0, +) % 1234567891
 }
 
 func r(_ i:Int) -> Int{
@@ -17,6 +17,7 @@ func r(_ i:Int) -> Int{
     if i > 0{
         for _ in 1...i{
             gye *= 31
+            gye %= 1234567891
         }
     }
     return gye
