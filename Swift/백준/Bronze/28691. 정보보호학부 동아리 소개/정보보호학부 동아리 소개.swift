9055d48a2acc74a48bdd80ed1b1414eaba8a1777
Ito <107340769+changjaemun@users.noreply.github.com>
2024-08-18 15:35:10 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 4 ms, Memory: 69096 KB -BaekjoonHub

---
+++ Swift/백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.swift
@@ -0,0 +1,16 @@
+let str = readLine()!
+
+switch str{
+case "M":
+    print("MatKor")
+case "W":
+    print("WiCys")
+case "C":
+    print("CyKor")
+case "A":
+    print("AlKor")
+case "$":
+    print("$clear")
+default:
+    print("")
+}
